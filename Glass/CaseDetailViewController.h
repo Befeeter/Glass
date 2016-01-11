304e4e31d8812485cc8abd7bac601d69ae71430f
Tarik <tarik@Mac-Pro-de-Tarik.local>
2016-01-11 09:54:24 -0800

    Ahi vamos probando

--- Glass/CaseDetailViewController.h
+++ Glass/CaseDetailViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface CaseDetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *foto;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *RIcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *NAcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *MGcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *ALcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *SIcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *Kcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *CAcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *BAcell;
+@property (weak, nonatomic) IBOutlet UICollectionViewCell *FEcell;
+
 
 @end
