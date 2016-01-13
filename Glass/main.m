62ed9969c4c512e12e3743d3e1cfde5b7bf7f554
Tarik <tarik@Mac-Pro-de-Tarik.local>
2016-01-13 14:39:53 -0800

    dfdf

--- Glass/main.m
+++ Glass/main.m
@@ -1,7 +1,8 @@
 //
 //  main.m
 //  Glass
-//
+//#9	0x000063dd in main at /Users/tarik/Desktop/Proyecto/Glass/Glass/main.m:16
+
 //  Created by Aula1 on 30/11/15.
 //  Copyright (c) 2015 grupo 04. All rights reserved.
 //
@@ -16,3 +17,4 @@ int main(int argc, char * argv[])
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+                                
