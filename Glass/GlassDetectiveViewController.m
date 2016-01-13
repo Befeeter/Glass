aa6d7a2fe1efa4e665bdecf70c6dbe1ad445aa4d
Jorge Martinez Carvajal <jorge_zk8@hotmail.com>
2016-01-13 18:09:17 +0100

    arreglo ejecucion

--- Glass/GlassDetectiveViewController.m
+++ Glass/GlassDetectiveViewController.m
@@ -14,6 +14,7 @@
 
 @implementation GlassDetectiveViewController
 
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
